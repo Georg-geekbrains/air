28d85dd87f11a5b1ecd14b0d6c9f319f62df4b61
Georg-geekbrains <120080723+Georg-geekbrains@users.noreply.github.com>
2024-03-24 19:09:38 +0300

    Update task_sem_6.py

--- dags/task_sem_6.py
+++ dags/task_sem_6.py
@@ -6,18 +6,18 @@ from datetime import datetime, timedelta
 import random
 import requests
 
-# Функция для генерации рандомного числа и печати его в консоль
+
 def print_random_number():
     random_number = random.randint(1, 100)
     print(f"Сгенерированное рандомное число: {random_number}")
 
-# Функция для генерации рандомного числа, возвода его в квадрат и вывода в консоль
+
 def generate_and_square_random_number():
     random_number = random.randint(1, 100)
     squared_number = random_number ** 2
     print(f"Исходное число: {random_number}, Результат: {squared_number}")
 
-# Функция для отправки запроса к API погоды и вывода информации о погоде
+
 def get_weather_and_print(location):
     url = f"https://goweather.herokuapp.com/weather/{location}"
     response = requests.get(url)
@@ -27,7 +27,7 @@ def get_weather_and_print(location):
     else:
         print("Не удалось получить данные о погоде.")
 
-# Определение аргументов по умолчанию
+
 default_args = {
     'owner': 'user',
     'depends_on_past': False,
@@ -38,7 +38,7 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-# Определение DAG
+
 dag = DAG(
     'random_number_weather_dag',
     default_args=default_args,
@@ -46,7 +46,7 @@ dag = DAG(
     schedule_interval='@once',
 )
 
-# Определение операторов
+
 generate_random_number_task = BashOperator(
     task_id='generate_random_number',
     bash_command='echo $((RANDOM % 100))',
@@ -66,5 +66,5 @@ get_weather_task = PythonOperator(
     dag=dag,
 )
 
-# Определение порядка выполнения задач
+
 generate_random_number_task >> generate_and_square_task >> get_weather_task
